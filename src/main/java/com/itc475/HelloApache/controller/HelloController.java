c9e977cb87ace2f38bf40d6fe8cae611fb0fe0dc
Justin Cupp <justincupp@MacBookPro.attlocal.net>
2025-04-19 14:36:18 -0500

    feature B

--- src/main/java/com/itc475/HelloApache/controller/HelloController.java
+++ src/main/java/com/itc475/HelloApache/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 	@GetMapping("/hello")
-	public String hello() {
+	public String hello_B() {
 		return "hello-form";
 	}
 
